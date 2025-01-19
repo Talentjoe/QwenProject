27d5b4c7d11318a531750c8b23ff1288e56b542f
Talentjoe <88471172+Talentjoe@users.noreply.github.com>
2025-01-18 20:42:51 -0800

    更改

---
+++ Assistant.py
@@ -0,0 +1,68 @@
+import time
+from datetime import datetime
+from time import strftime
+import json
+
+import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer,StoppingCriteriaList,StoppingCriteria
+from transformers import TextStreamer
+device = "cuda" # the device to load the model onto
+
+name = "Qwen/Qwen2.5-3B-Instruct"
+
+messages = ([
+        {"role": "system", "content": "You are a helpful assistant."},
+    ])
+
+
+# Now you do not need to add "trust_remote_code=True"
+model = AutoModelForCausalLM.from_pretrained(
+    name,
+    torch_dtype="auto",
+    device_map="auto"
+)
+tokenizer = AutoTokenizer.from_pretrained(name)
+if tokenizer.pad_token_id is None:
+    tokenizer.pad_token = tokenizer.eos_token  # Set pad token if missing
+    tokenizer.pad_token_id = tokenizer.eos_token_id + 1  # Assign a unique ID for padding
+
+
+
+while True:
+    # Instead of using model.chat(), we directly use model.generate()
+    # But you need to use tokenizer.apply_chat_template() to format your inputs as shown below
+    prompt = input("You: ")
+    if prompt == "bye":
+        break
+    messages.append({"role": "user", "content": prompt})
+
+    print(messages)
+
+    text = tokenizer.apply_chat_template(
+        messages,
+        tokenize=False,
+        add_generation_prompt=True
+    )
+    model_inputs = tokenizer([text], return_tensors="pt").to(device)
+
+    # Directly use generate() and tokenizer.decode() to get the output.
+    # Use `max_new_tokens` to control the maximum output length.
+    streamer = TextStreamer(tokenizer, skip_prompt=True, skip_special_tokens=True)
+
+
+    print("Qwen: ",end="" )
+    generated_ids = model.generate(
+        model_inputs.input_ids,
+        max_new_tokens=512,
+        streamer=streamer,
+    )
+    #print(tokenizer.decode(generated_ids[0], skip_special_tokens=True))
+    messages.append({"role": "Qwen", "content": tokenizer.decode(generated_ids[0], skip_special_tokens=True).split("assistant")[-1]})
+    print("----end----")
+
+path = r"ChatHistory\chat_history_"+strftime("%Y-%m-%d_%H-%M-%S")
+with open(path+ ".txt", "w", encoding='utf-8') as f:
+    for message in messages:
+        f.write(f"{message['role']}: {message['content']}\n")
+with open(path + ".json", "w", encoding='utf-8') as f:
+    json.dump(messages, f, ensure_ascii=False, indent=4)
---
+++ TwoBots.py
@@ -0,0 +1,51 @@
+import time
+import os
+os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
+from transformers import AutoModelForCausalLM, AutoTokenizer
+device = "cuda" # the device to load the model onto
+
+
+test_time = time.strftime("%Y-%m-%d_%H-%M-%S")
+# Now you do not need to add "trust_remote_code=True"
+model = AutoModelForCausalLM.from_pretrained(
+    "Qwen/Qwen2.5-3B-Instruct",
+    torch_dtype="auto",
+    device_map="auto"
+)
+tokenizer = AutoTokenizer.from_pretrained("Qwen/Qwen2.5-3B-Instruct")
+
+role = "A"
+ChatHistory = [{"role":"A", "content": "你好"}, {"role":"B", "content": "你好，让我们聊聊历史文学方面的话题吧"}]
+
+while True:
+    messages = [
+        {"role": "system", "content": "你是一个人类，名字为 "+role+" ，现在请你和另一个人聊天，请表现的尽可能自然 你们可以聊任何方面的话题。"},
+    ]
+    messages+= ChatHistory
+    text = tokenizer.apply_chat_template(
+        messages,
+        tokenize=False,
+        add_generation_prompt=True
+    )
+    model_inputs = tokenizer([text], return_tensors="pt").to(device)
+
+    from transformers import TextIteratorStreamer
+    streamer = TextIteratorStreamer(tokenizer, skip_prompt=True, skip_special_tokens=True)
+
+    from threading import Thread
+    generation_kwargs = dict(model_inputs, streamer=streamer, max_new_tokens=512)
+    thread = Thread(target=model.generate, kwargs=generation_kwargs)
+
+    thread.start()
+    generated_text = ""
+    for new_text in streamer:
+        generated_text += new_text
+        print(new_text,end = "")
+    print("end",role)
+    ChatHistory.append({"role":role, "content": generated_text})
+
+    role = "B" if role == "A" else "A"
+
+
+    with open("generated_text"+test_time+".txt", "a",encoding="utf-8") as f:
+        f.write(role+": "+generated_text+"\n")
