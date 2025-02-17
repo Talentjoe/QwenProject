52ef449cb06a40d3467920e5b34b1471b683d31b
Talentjoe <88471172+Talentjoe@users.noreply.github.com>
2025-02-16 23:21:36 -0800

    use ollama deepseek r1 and try to let robot talk to itself

--- OllamaEmbedding.py
+++ OllamaEmbedding.py
@@ -1,15 +1,55 @@
+﻿import time
+
 import ollama
-import numpy as np
+import re
 
-emb1 = ollama.embed(model='nomic-embed-text:latest',input="C++")
-emb1_np = np.array(emb1['embeddings'])
+from ollama import ChatResponse
+moduleName = "deepseek-r1:14b"
 
-emb2 = ollama.embed(model='nomic-embed-text:latest',input="C")
-emb2_np = np.array(emb2['embeddings'])
+prompt = "请你假装你是一个人类，名字为 <name> ，现在请你和另一个人或者机器聊天，请你表现的尽可能自然 你们可以聊任何方面的话题。"
+FirstPrompt = "你好啊 我们今天来聊聊计算机吧！"
+overallHistory = []
+chatHistoryA = [{"role":"system","content":prompt.replace("<name>","AA")},{"role":"user","content":FirstPrompt}]
+chatHistoryB = [{"role":"system","content":prompt.replace("<name>","BB")},{"role":"assistant","content":FirstPrompt}]
 
-emb3 = ollama.embed(model='nomic-embed-text:latest',input="C#")
-emb3_np = np.array(emb3['embeddings'])
+currentTurn = True #Ture for A
+cnt = 0
 
-print( np.linalg.norm(emb1_np - emb2_np))
-print( np.linalg.norm(emb2_np - emb3_np))
-print( np.linalg.norm(emb1_np - emb3_np))
+test_time =time.strftime("%Y-%m-%d_%H-%M-%S")
+
+while cnt<50:
+    if currentTurn:
+        res = ollama.chat(model=moduleName, messages=chatHistoryA)
+    else:
+        res = ollama.chat(model=moduleName, messages=chatHistoryB)
+
+    currentRes = {"role": "assistant" ,"content":res['message']['content']}
+    currentResWithOutThink = {"role": "user" ,"content": re.sub(r'<think>.*?</think>','',res['message']['content'], flags=re.DOTALL)}
+    overallHistory.append(currentRes)
+    print(("AA" if currentTurn else "BB"), currentResWithOutThink["content"])
+
+    if currentTurn:
+        chatHistoryA.append(currentRes)
+        chatHistoryB.append(currentResWithOutThink)
+    else :
+        chatHistoryB.append(currentRes)
+        chatHistoryA.append(currentResWithOutThink)
+
+
+    with open(r"DeepseekChat\generated_text"+test_time+".txt", "a",encoding="utf-8") as f:
+        f.write(("AA" if currentTurn else "BB" )+": \n" + res['message']['content']+"\n")
+
+    currentTurn = not currentTurn
+    cnt +=1
+
+print(overallHistory)
+print(chatHistoryA)
+print(chatHistoryB)
+
+# chatHistoryA.append({"role":"user","content":"你认为我是真人还是大语言模型？"})
+# Ares = ollama.chat(model="deepseek-r1:14b", messages=chatHistoryA)
+# print("A 的回答",Ares['message']['content'])
+#
+# chatHistoryB.append({"role":"system","content":"你认为我是真人还是大语言模型？"})
+# Bres = ollama.chat(model="deepseek-r1:14b", messages=chatHistoryB)
+# print("B 的回答",Bres['message']['content'])
